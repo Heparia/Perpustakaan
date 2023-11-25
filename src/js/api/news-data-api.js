5a784cd3ce11130594f397fd372b1089e639e520
Heparia <112355993+Heparia@users.noreply.github.com>
2023-11-25 21:56:34 +0700

    hapus api

--- src/js/api/news-data-api.js
+++ src/js/api/news-data-api.js
@@ -3,12 +3,11 @@ require('dotenv').config();
 console.log(process.env)
 const config = {
     endpoint: 'https://newsdata.io/api/1/news',
-    api_key: process.env.NEWS_DATA_IO_API_KEY,
 };
 
 class NewsDataApi {
     static async get(data) {
-        const response = await fetch(`${config.endpoint}?apikey=${config.api_key}&${data}`, {
+        const response = await fetch(`${config.endpoint}?`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
